7c4fe4f07795c1b8b5fe65cda4c70746e29a5dfb
jakednich <jakenichols57@gmail.com>
2025-02-12 08:20:35 -0600

    Added a prompt to get the human move choice. Also wrote the logic for the winner and loser in the rps game.

--- script.js
+++ script.js
@@ -1,12 +1,52 @@
+// Computer Move Choice
 function getComputerChoice() {
     let computerChoice = Math.floor(Math.random()*3) // gives us a number 0 to 2
-    console.log(computerChoice)
     
     if (computerChoice === 0) {
-        computerChoice = 'Rock'
+        computerChoice = 'R'
     } else if (computerChoice === 1) {
-        computerChoice = 'Paper'
+        computerChoice = 'P'
     } else {
-        computerChoice = 'Scissors'
+        computerChoice = 'S'
+    }
+
+    return getComputerChoice
+}
+
+// Human Move Choice
+function getHumanChoice() {
+    let humanChoice = prompt('Rock, Paper, Scissors, Shoot! Enter your move: ')
+    humanChoice = humanChoice.charAt(0).toUpperCase()
+    
+    return humanChoice
+}
+
+// Score intialized
+let humanScore = 0
+let computerScore = 0
+let tieCounter = 0
+
+// single round
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice){
+        console.log('It is a tie!')
+        tieCounter += 1
+    } else if (
+        (humanChoice === 'R' && computerChoice === 'P') ||
+        (humanChoice === 'P' && computerChoice === 'S') ||
+        (humanChoice === 'S' && computerChoice === 'R')
+    ){
+        console.log(`You lost. ${computerChoice} beats ${humanChoice}.`)
+        computerScore += 1
+    } else if (
+        (humanChoice === 'R' && computerChoice === 'S') ||
+        (humanChoice === 'P' && computerChoice === 'R') ||
+        (humanChoice === 'S' && computerChoice === 'P')
+    ){
+        console.log(`You won! ${humanChoice} beats ${computerChoice}.`)
+        humanScore += 1
+    } else {
+        console.log(`Invalid input. Press enter to try again.`)
     }
 }
